25112473547c27be06b7261045f904535c4acab2
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-24 22:01:56 +0200

    Added resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -152,7 +152,9 @@ const Navbar = () => {
 					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-gray-600'>
 						<a
 							className='flex justify-between items-center w-full text-gray-300 pl-2'
-							href='/'
+							href='https://docs.google.com/document/d/e/2PACX-1vQ_Q7c1hKV9VedSMWvwbuDS1ZTc2gJErlInKB7xclqhlmA19cJji3j5WczkGH2RKBLi_8hqIpUi73LN/pub'
+							rel='norefferer'
+							target='_blank'
 						>
 							Resume <BsFillPersonLinesFill size={30} />
 						</a>
