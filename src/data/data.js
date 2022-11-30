39236df817a5e5e1341104cd047504273ccbb3d3
ersin-ahmed <ersinahmeddev@gmail.com>
2022-11-30 23:48:46 +0200

    Added new project

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import Blog from '../assets/projects/blog.png';
 import Portfolio from '../assets/projects/portfolio.png';
+import Contact from '../assets/projects/contactmanager.png';
 
 export const data = [
 	{
@@ -15,6 +16,17 @@ export const data = [
 	},
 	{
 		id: 2,
+		name: 'Contact Management App (MERN)',
+		description:
+			'A Full Stack Web App with full CRUD and authentication built using the popular MERN stack (MongoDB, Express, React, Node). For styling I used TailwindCSS. Deployment was made through render.com, the client side as a static website and the backend as an express api web service. The build tool used on the front-end is Vite.',
+		image: Contact,
+		github: 'https://github.com/ersin-ahmed/contact-manager-mern-client',
+		live: 'https://contact-manager-mern.onrender.com',
+		stack:
+			'JavaScript, React, Vite, TailwindCSS, Node, Express, MongoDB, MongoDB Atlas',
+	},
+	{
+		id: 3,
 		name: 'Portfolio Site',
 		description:
 			"This is the very same Portfolio Site you're currently on at the moment. This is where I showcase my projects that i've built along with additional information about myself. I made this website using React with Vite as the building tool for the framework. And I used Tailwind CSS for the styling.",
